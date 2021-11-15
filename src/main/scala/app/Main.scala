5d0b8d4cefa29e9666db266ead3ecd5e69b0afd4
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2021-11-16 00:57:51 +0900

    デバイス名を指定して開ける

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,42 +1,49 @@
 package app
 
-import javax.sound.midi.{ MidiEvent, MidiSystem, Sequence, ShortMessage }
+import javax.sound.midi.{ MidiDevice, MidiEvent, MidiSystem, Sequence, Sequencer, ShortMessage, Synthesizer }
 
 object Main extends App {
   val midiDevices = MidiSystem.getMidiDeviceInfo
+    .map(info => (info, MidiSystem.getMidiDevice(info)))
+    .filter { case (_, midiDevice) => !midiDevice.isInstanceOf[Sequencer] && !midiDevice.isInstanceOf[Synthesizer] }
+  val midiOutputDevices = midiDevices
+    .filter { case (_, midiDevice) => midiDevice.getMaxReceivers != 0 }
+  val outputDeviceOpt = midiOutputDevices
+    .find { case (midiDeviceInfo, _) => midiDeviceInfo.getName == "loopMIDI Port 1" }
+  val midiInputDevices = midiDevices
+    .filter { case (_, midiDevice) => midiDevice.getMaxTransmitters != 0 }
 
-  // デバイスを表示してみるテスト
-  midiDevices.foreach { midiDeviceInfo =>
-    val midiDevice = MidiSystem.getMidiDevice(midiDeviceInfo)
-
-    val message =
-      s"""instance type : ${midiDevice.getClass.getName}
-         | name : ${midiDeviceInfo.getName}
-         | vendor : ${midiDeviceInfo.getVendor}
-         | description : ${midiDeviceInfo.getDescription}
-         | version : ${midiDeviceInfo.getVersion}""".stripMargin
-
-    println(message)
+  def midiDeviceInfoStr(midiDeviceInfo: MidiDevice.Info): String = {
+    s"""name : ${midiDeviceInfo.getName}
+       |vendor : ${midiDeviceInfo.getVendor}
+       |description : ${midiDeviceInfo.getDescription}
+       |version : ${midiDeviceInfo.getVersion}""".stripMargin
   }
 
-  val receiver = MidiSystem.getReceiver
+  outputDeviceOpt.fold {} {
+    case (midiDeviceInfo, midiDevice) =>
+      println("connected")
+      println(midiDeviceInfoStr(midiDeviceInfo))
 
-  val sequencer = MidiSystem.getSequencer
-  sequencer.getTransmitter.setReceiver(receiver)
+      midiDevice.open()
+      val receiver = midiDevice.getReceiver
 
-  // 適当なシーケンスを作る
-  val sequence = new Sequence(Sequence.PPQ, 480)
-  val track = sequence.createTrack()
+      val sequencer = MidiSystem.getSequencer(false)
+      val transmitter = sequencer.getTransmitter
+      transmitter.setReceiver(receiver)
 
-  val noteOn = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 93)
-  val noteOff = new ShortMessage(ShortMessage.NOTE_OFF, 0, 60, 0)
-  track.add(new MidiEvent(noteOn, 480))
-  track.add(new MidiEvent(noteOff, 480 * 2))
+      val sequence = new Sequence(Sequence.PPQ, 480)
+      val track = sequence.createTrack()
+      val noteOn = new ShortMessage(ShortMessage.NOTE_ON, 0, 36, 127)
+      val noteOff = new ShortMessage(ShortMessage.NOTE_OFF, 0, 36, 0)
+      track.add(new MidiEvent(noteOn, 480))
+      track.add(new MidiEvent(noteOff, 480 * 2))
 
-  sequencer.open()
-  sequencer.setSequence(sequence)
-  sequencer.start()
-  sequencer.setLoopCount(10)
-  Thread.sleep(960 * 10)
-  sequencer.close()
+      sequencer.open()
+      sequencer.setSequence(sequence)
+      sequencer.start()
+      sequencer.setLoopCount(10)
+      Thread.sleep((sequencer.getMicrosecondLength / 1000) * 10)
+      sequencer.close()
+  }
 }
