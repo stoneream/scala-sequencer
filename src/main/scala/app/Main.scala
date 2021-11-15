7dae7e6e98f5437e1b29a1015a8f7c7aedd27efd
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2021-11-15 17:06:21 +0900

    デバイスを表示してみるテスト

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,5 +1,19 @@
 package app
 
-object Main extends App {
+import javax.sound.midi.MidiSystem
 
+object Main extends App {
+  // デバイスを表示してみるテスト
+  MidiSystem.getMidiDeviceInfo.foreach { midiDeviceInfo =>
+    val midiDevice = MidiSystem.getMidiDevice(midiDeviceInfo)
+
+    val message =
+      s"""instance type : ${midiDevice.getClass.getName}
+         | name : ${midiDeviceInfo.getName}
+         | vendor : ${midiDeviceInfo.getVendor}
+         | description : ${midiDeviceInfo.getDescription}
+         | version : ${midiDeviceInfo.getVersion}""".stripMargin
+
+    println(message)
+  }
 }
